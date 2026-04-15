c1df32f3df5cb750771ef122524b1e1602d58107
Olawoyin Joseph Folasakin <olawoyinjoseph05@gmail.com>
2026-04-15 15:58:09 +0100

    Refactor customer schema: re-enable firstName and lastName fields, comment out phoneNumber requirement

--- models/user.model.js
+++ models/user.model.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose")
 
 let customerSchema = mongoose.Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
     email: {type: String, required: [true, 'Email has been taken, please choose another one']},
-    phoneNumber: {type: String, required: [true, 'User phone number is required']},
+    // phoneNumber: {type: String, required: [true, 'User phone number is required']},
     password: {type: String, required: true}
 })
 
